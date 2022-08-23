f5beb2b18398806e55df491e6135496d7bb8d225
Tomas Delmastro <105322308+tomasdelmastro@users.noreply.github.com>
2022-08-23 17:14:49 -0300

    Update Login-Home
    Todos los url conectados, falta conectar con la base de datos y que corrobore que esta logueado.

--- itbank/home/urls.py
+++ itbank/home/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from Login.views import sign_out
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dolar/', views.dollar, name='dolar'),
-    path('sign-out/', sign_out, name='sign out'),
+    path('Error404/', views.Error404, name='error404'),
 ]
--- itbank/home/views.py
+++ itbank/home/views.py
@@ -1,3 +1,4 @@
+from urllib.robotparser import RequestRate
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
@@ -6,10 +7,13 @@ def home(request):
     return render(request, 'home/index.html')
 
 
-@login_required(login_url='/login/')
+#@login_required(login_url='/login/')
 def dashboard(request):
     return render(request, 'home/dashboard.html')
 
 
 def dollar(request):
     return render(request, 'home/dollar.html')
+
+def Error404(request):
+    return render(request, 'home/Error404.html')
