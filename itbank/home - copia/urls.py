b5aaaa50903f63d43f55e56f08c0b79cedc4b76a
Santiago <70781480+Sancho6446@users.noreply.github.com>
2022-08-23 20:21:11 -0300

    Delete itbank/home - copia directory

--- itbank/home - copia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dolar/', views.dollar, name='dolar'),
-    path('Error404/', views.Error404, name='error404'),
-]
