34ac23c1e4bd8331afad31a4ba4d4d5503f675b6
Tomas Delmastro <105322308+tomasdelmastro@users.noreply.github.com>
2022-08-25 18:39:39 -0300

    No se q cambie xd

--- itbank/Login/views.py
+++ itbank/Login/views.py
@@ -8,10 +8,6 @@ from django.contrib.auth import authenticate, login, logout
 from cuentas.models import Cliente, Cuenta
 from login.models import Usuario
 
-#from .models import Room, Topic
-#from .forms import RoomForm
-
-
 
 def create_context(username):
     customer_id = Usuario.objects.get(
@@ -57,44 +53,3 @@ def loginPage(request):
 def logoutPage(request):
     logout(request)
     return redirect('home')
-
-def existe_usuario(data):
-    try:
-        return Usuario.objects.get(Q(customer_id=data["customer_id"]) | Q(username=data['username']))
-    except Usuario.DoesNotExist:
-        return False
-
-
-def is_valid(customer_id):
-    try:
-        return Cliente.objects.get(customer_id=customer_id)
-    except Cliente.DoesNotExist:
-        return False
-
-
-def sign_up(request):
-    if request.method == 'POST':
-        customer_id = request.POST['customer_id']
-        if is_valid(customer_id):
-            if existe_usuario(request.POST):
-                return render(request, 'login/login.html')
-            else:
-                username = request.POST['username']
-                password = request.POST['password']
-                email = request.POST['email']
-                usuario = Usuario.objects.create_user(
-                    username=username, password=password, email=email)
-                usuario.customer_id = customer_id
-                usuario.save()
-                user = authenticate(
-                    request, username=username, password=password)
-
-                if user:
-                    login(request, user)
-
-                    context = create_context(username)
-                    return render(request, 'homepage/dashboard.html', context)
-
-        return render(request, 'login/login.html')
-
-
