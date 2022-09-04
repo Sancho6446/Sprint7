937049d89aa1b10308fe7b12180a55e41e2da340
Agustín <105322348+agstino@users.noreply.github.com>
2022-09-04 15:40:58 -0300

    Update models.py

--- itbank/login/models.py
+++ itbank/login/models.py
@@ -1,12 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from cuentas.models import Cliente
+
 
 # Create your models here.
-
-class Usuario(User):
-    customer = models.OneToOneField(
-        Cliente, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.username
